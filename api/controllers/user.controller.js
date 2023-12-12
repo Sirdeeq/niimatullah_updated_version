06a4b7c8a4b112f7e3e0e5fb46914716c384bb99
sirdeeq <ssirdeeq@gmail.com>
2023-12-12 20:22:11 +0100

    save

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -20,7 +20,7 @@ export const updateUser = async (req, res, next) => {
     const updatedUser = await User.findByIdAndUpdate(
       req.params.id,
       {
-        ₦set: {
+        $set: {
           username: req.body.username,
           email: req.body.email,
           password: req.body.password,
